6b9d52e27a506cde2235bc60adda694ccd5c9e44
Pooj0605 <kanna.p@northeastern.edu>
2024-02-23 19:56:32 -0500

    adding VM instances to the code

--- provider.tf
+++ provider.tf
@@ -3,3 +3,40 @@ provider "google" {
   project = var.project_id
   region  = var.region
 }
+
+resource "google_compute_instance" "vm_instance" {
+  for_each = var.instances_configuration
+
+  name         = each.value.name
+  machine_type = each.value.machine_type
+  zone         = each.value.zone
+
+  boot_disk {
+    initialize_params {
+      image = each.value.boot_image
+      type  = each.value.boot_disk_type
+      size  = each.value.boot_disk_size_gb
+    }
+  }
+
+  network_interface {
+    network    = each.value.network
+    subnetwork = each.value.subnetwork
+    // Assuming your VPC and subnets are already configured
+  }
+
+  service_account {
+    email  = var.service_account_email
+    scopes = ["https://www.googleapis.com/auth/cloud-platform"]
+  }
+
+  tags = each.value.tags
+
+  metadata = {
+    // Custom metadata can be added here
+  }
+
+  lifecycle {
+    prevent_destroy = false
+  }
+}
--- variables.tf
+++ variables.tf
@@ -17,37 +17,37 @@ variable "vpcs" {
   }))
 }
 
-# variable "zone" {
-#   description = "The GCP zone to deploy resources into"
-#   type        = string
-# }
+variable "zone" {
+  description = "The GCP zone to deploy resources into"
+  type        = string
+}
 
-# variable "custom_image_self_link" {
-#   description = "The self_link of the custom image built with Packer"
-#   type        = string
-# }
+variable "custom_image_self_link" {
+  description = "The self_link of the custom image built with Packer"
+  type        = string
+}
 
-# variable "vpc_self_link" {
-#   description = "The self_link of the VPC where the instance will be deployed"
-#   type        = string
-# }
+variable "vpc_self_link" {
+  description = "The self_link of the VPC where the instance will be deployed"
+  type        = string
+}
 
-# variable "service_account_email" {
-#   description = "The email of the service account for the instance"
-#   type        = string
-# }
+variable "service_account_email" {
+  description = "The email of the service account for the instance"
+  type        = string
+}
 
-# variable "instances_configuration" {
-#   description = "A map of instance configurations"
-#   type = map(object({
-#     name                = string
-#     machine_type        = string
-#     zone                = string
-#     network             = string
-#     subnetwork          = string
-#     boot_image          = string
-#     boot_disk_type      = string
-#     boot_disk_size_gb   = number
-#     tags                = list(string)
-#   }))
-# }
+variable "instances_configuration" {
+  description = "A map of instance configurations"
+  type = map(object({
+    name                = string
+    machine_type        = string
+    zone                = string
+    network             = string
+    subnetwork          = string
+    boot_image          = string
+    boot_disk_type      = string
+    boot_disk_size_gb   = number
+    tags                = list(string)
+  }))
+}

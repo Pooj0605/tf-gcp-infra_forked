d0e9c7a57017ca512d9d00d384307b491ee16823
Pooj0605 <kanna.p@northeastern.edu>
2024-03-20 20:35:56 -0400

    made changes on main.tf file

--- main.tf
+++ main.tf
@@ -7,17 +7,14 @@ provider "google" {
 }
 
 data "template_file" "metadata_script" {
-  
- template= file("${path.module}/script.sh")
- vars={ 
-  DB_HOST ="${google_sql_database_instance.sql_instance_terraform.private_ip_address}"
-  DB_USER="${google_sql_user.user_terraform.name}"
-  DB_PASSWORD="${random_password.password_terraform.result}"
-  DB_NAME="${google_sql_database.terraform_database.name}"
+
+  template = file("${path.module}/script.sh")
+  vars = {
+    DB_HOST     = "${google_sql_database_instance.sql_instance_terraform.private_ip_address}"
+    DB_USER     = "${google_sql_user.user_terraform.name}"
+    DB_PASSWORD = "${random_password.password_terraform.result}"
+    DB_NAME     = "${google_sql_database.terraform_database.name}"
   }
-  
-  
-    
 }
 
 resource "google_compute_instance" "terraform_instance" {
@@ -35,10 +32,49 @@ resource "google_compute_instance" "terraform_instance" {
   network_interface {
     subnetwork = google_compute_subnetwork.terraform_subnet["subnet1"].self_link
     access_config {}
-
-    
-    
   }
+  metadata_startup_script = data.template_file.metadata_script.rendered
 
- metadata_startup_script = data.template_file.metadata_script.rendered
+  service_account {
+    email  = google_service_account.service_account.email
+    scopes = ["logging-write", "monitoring-write"]
+  }
+}
+
+
+
+# //SERVICE ACCOUNT CREATION 
+resource "google_service_account" "service_account" {
+  account_id   = var.serviceid
+  display_name = var.serviceaccname
+}
+
+//SERVICE ACCOUNT ROLES SETUP 
+
+resource "google_project_iam_binding" "service_account_logging_admin" {
+  project = var.project_id
+  role    = "roles/logging.admin"
+
+  members = [
+    "serviceAccount:${google_service_account.service_account.email}"
+  ]
+}
+
+resource "google_project_iam_binding" "service_account_metric_writer" {
+  project = var.project_id
+  role    = "roles/monitoring.metricWriter"
+
+  members = [
+    "serviceAccount:${google_service_account.service_account.email}"
+  ]
+}
+
+
+//DNS A-RECORD SETUP
+resource "google_dns_record_set" "dns-a-record-terraform" {
+  name         = var.dns-name
+  type         = var.recordtype
+  ttl          = var.ttl
+  managed_zone = var.managedzonename
+  rrdatas      = [google_compute_instance.terraform_instance.network_interface[0].access_config[0].nat_ip]
 }
